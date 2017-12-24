bdfce3557dafc85e9c79f360e358321a440f9668
Ivan Diaz <feleir@gmail.com>
2017-12-24 10:51:33 +0100

    Updated list contacts UI

--- src/ListContacts.js
+++ src/ListContacts.js
@@ -5,7 +5,18 @@ class ListContacts extends Component {
         return (
             <ol className='contact-list'>
                 {this.props.contacts.map(contact => (
-                    <li>{contact.name}</li>
+                    <li key={contact.id} className='contact-list-item'>
+                        <div className='contact-avatar' style={{
+                            backgroundImage: `url(${contact.avatarURL})`
+                        }}/>
+                        <div className='contact-details'>
+                            <p>{contact.name}</p>
+                            <p>{contact.email}</p>
+                        </div>
+                        <button className='contact-remove'>
+                            Remove
+                        </button>
+                    </li>
                 ))}
             </ol>
         );
